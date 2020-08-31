4f21fae4ffd5f7f43e96e231202abd879624d916
fabiodc8 <dc.fabio@icloud.com>
2020-08-31 11:10:02 +0000

    commit2

---
+++ ui/webapp/controller/Ore.controller.js
@@ -0,0 +1,45 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("ui.ui.controller.Ore", {
+
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 * @memberOf ui.ui.view.Ore
+		 */
+		onInit: function () {
+
+		},
+
+		/**
+		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+		 * (NOT before the first rendering! onInit() is used for that one!).
+		 * @memberOf ui.ui.view.Ore
+		 */
+		//	onBeforeRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+		 * This hook is the same one that SAPUI5 controls get after being rendered.
+		 * @memberOf ui.ui.view.Ore
+		 */
+		//	onAfterRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+		 * @memberOf ui.ui.view.Ore
+		 */
+		//	onExit: function() {
+		//
+		//	}
+
+	});
+
+});
